54316107b12cf3e536e77b62973c4e499b487ad4
Akinori Tomita <tommy@328w.co.jp>
2013-05-17 12:08:52 +0900

    Version 0.0.5 (2011-02-08)
    Compatible with Redmine 0.9.x, 1.0.x, 1.1.x.

--- App/controllers/issues_controller.rb
+++ App/controllers/issues_controller.rb
@@ -1,10 +1,16 @@
 require 'redmine'
 require_dependency 'issues_controller'
+require 'query_per_project_constants'
 
 class IssuesController < ApplicationController
 
   # Retrieve query from session or build a new query
 	def retrieve_query_with_default_query
+    # workaround bug in homepage plugin (if set to query_id, not possible to change custom at all)
+    if !@project && params[:set_filter] && params[:query_id]
+      params.delete(:query_id)
+    end
+    #
 		if should_apply_for_default_query?
   		get_default_query
  			retrieve_query_without_default_query unless @query
@@ -19,8 +25,9 @@ private
 
 	def should_apply_for_default_query?
 		return false unless params[:query_id].blank?
-		if (params[:set_filter] || session[:query].nil? || 
-			   	session[:query][:project_id] != (@project ? @project.id : nil))
+		if (api_request? || params[:set_filter] || session[:query].nil? || 
+			   	session[:query][:project_id] != (@project ? @project.id : nil) ||
+				session[:query][:column_names].nil? )
 			return true
 	  end
 		return false
@@ -28,30 +35,38 @@ private
 	
   def get_default_query
     @query = nil
-    if params[:set_filter].nil? and @project
-    	query_default_name = Setting.plugin_redmine_default_columns['default_query_name']
-      @query = Query.find_by_name(query_default_name, :conditions => "project_id = #{@project.id}")
-      if !@query
-      	prj = Project.find_by_id(Setting.plugin_redmine_default_columns['query_templates_project_id'])
-        if prj
-          template_str = "Project"
-          custom_name = Setting.plugin_redmine_default_columns['type_custom_field_name']
-          @project.custom_values.each do |custom_value|
-            if custom_value.custom_field.name == custom_name and !custom_value.value.blank?
-              template_str = custom_value.value.to_s.strip
-              break
+    if params[:set_filter].nil?
+      prj = Project.find_by_id(Setting.plugin_redmine_default_columns['query_templates_project_id'])
+      custom_name = Setting.plugin_redmine_default_columns['type_custom_field_name']
+      if @project
+        query_default_name = Setting.plugin_redmine_default_columns['my_default_query_name']
+        @query = Query.find_by_name(query_default_name, :conditions => "project_id = #{@project.id} AND user_id = #{User.current.id}")
+        unless @query
+          query_default_name = Setting.plugin_redmine_default_columns['default_query_name']
+          @query = Query.find_by_name(query_default_name, :conditions => "project_id = #{@project.id}")
+        end
+        unless @query
+          if prj
+            cv=@project.custom_values.detect do |custom_value|
+              true if custom_value.custom_field.name == custom_name and !custom_value.value.blank?
             end
+            template_str = ( cv ? cv.value.to_s.strip : QPP_Constants::PROJECT_DEFAULT_SUFFIX )
+            @query = Query.find_by_name(QPP_Constants::QUERY_DEFAULT_PFX + template_str, :conditions => "project_id = #{prj.id}" )
           end
-          template_str = "Default_" + template_str
-          @query = Query.find_by_name(template_str, :conditions => "project_id = #{prj.id}" )
+        end
+      else
+        query_default_name = Setting.plugin_redmine_default_columns['my_global_query_name']
+        @query = Query.find_by_name(query_default_name, :conditions => "project_id IS NULL AND user_id = #{User.current.id}")
+        unless @query
+          @query = Query.find_by_name(QPP_Constants::QUERY_DEFAULT_GLOBAL_NAME, :conditions => "project_id IS NULL" ) if prj
         end
       end
-      
       if @query
         @query = @query.clone
         @query.project = @project
-        @query.project_id = @project.id
+        @query.name=QPP_Constants::QUERY_TEMP_NAME
         session[:query] = {:project_id => @query.project_id, :filters => @query.filters, :group_by => @query.group_by, :column_names => @query.column_names}
+        session[sort_name]=@query.sort_criteria.to_param
       end
     end
   end
--- init.rb
+++ init.rb
@@ -1,18 +1,24 @@
 require 'redmine'
+require 'query_per_project_constants'
 
-Redmine::Plugin.register :redmine_default_columns do
-  name 'Default columns'
-  author 'Vitaly Klimov'
-  author_url 'mailto:vvk@snowball.ru'
-  description 'Plugin allows to apply default queries for individual project or for projects based on their type.'
-  version '0.0.2'
-  
-  settings(:partial => 'settings/default_columns_settings',
-           :default => {
-             'query_templates_project_id' => '0',
-             'type_custom_field_name' => 'Type',
-             'default_query_name' => 'Default'
-           })
-  
+unless Redmine::Plugin.registered_plugins.keys.include?(:redmine_default_columns)
+  Redmine::Plugin.register :redmine_default_columns do
+    name 'Default queries per project'
+    author 'Vitaly Klimov'
+    author_url 'mailto:vvk@snowball.ru'
+    description 'Plugin allows to apply default queries for individual project or for projects based on their type.'
+    version '0.0.5'
 
+    settings(:partial => 'settings/default_columns_settings',
+             :default => {
+               'query_templates_project_id' => '0',
+               'type_custom_field_name' => QPP_Constants::TYPE_CUSTOM_FIELD_NAME,
+               'default_query_name' => QPP_Constants::QUERY_DEFAULT_NAME,
+               'my_default_query_name' => QPP_Constants::MY_QUERY_DEFAULT_NAME,
+               'my_global_query_name' => QPP_Constants::MY_QUERY_GLOBAL_DEFAULT_NAME,
+               'my_home_query_name' => QPP_Constants::MY_QUERY_HOME_NAME
+             })
+
+
+  end
 end
